fda2dab7641c05fde5059f65f271e7e7380c0762
ixhsan <muharipan@gmail.com>
2023-04-06 06:26:07 +0700

    update dependency package version

--- src/index.js
+++ src/index.js
@@ -1,22 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import reportWebVitals from "./reportWebVitals";
 import UserBox from "./components/UserBox";
-import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-} from "@apollo/client";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "http://localhost:3139/graphql",
+  uri: "http://localhost:3039/graphql",
   cache: new InMemoryCache({
-    dataIdFromObject:(data) => {
-      if (typeof data === 'object' && data.hasOwnProperty('success')) {
-        console.log(data, 'ini data full');
+    dataIdFromObject: (data) => {
+      if (typeof data === "object" && data.hasOwnProperty("success")) {
+        console.log(data, "ini data full");
       }
-    }
+    },
   }),
 });
 
@@ -28,8 +22,3 @@ root.render(
     </ApolloProvider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
